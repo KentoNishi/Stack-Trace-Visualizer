1a467678a9a6417dedd67dac66de0976dc129ff3
Kento Nishi <kento24gs@outlook.com>
2020-04-30 00:55:27 -0700

    Added improved GUI

--- src/StackEvent.java
+++ src/StackEvent.java
@@ -1,6 +1,16 @@
+import java.time.Duration;
+import java.time.Instant;
+
 public class StackEvent {
     private String eventMethod;
     private String returnValue;
+    private Instant startTime;
+    private Instant endTime;
+    private long timeTaken;
+
+    public StackEvent() {
+        this.startTime = Instant.now();
+    }
 
     public String getEventMethod() {
         return eventMethod;
@@ -15,7 +25,13 @@ public class StackEvent {
     }
 
     public void setReturnValue(String returnValue) {
+        this.endTime = Instant.now();
+        this.timeTaken = Duration.between(this.startTime, this.endTime).toMillis();
         this.returnValue = returnValue;
+    }
+
+    public long getTime() {
+        return this.timeTaken;
     }
 
     public String toString() {
--- src/TreeGUI.java
+++ src/TreeGUI.java
@@ -1,42 +1,51 @@
 import java.util.Stack;
+
 import javax.swing.ImageIcon;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTree;
+import javax.swing.KeyStroke;
+import javax.swing.SwingConstants;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.DefaultTreeModel;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 public class TreeGUI extends JFrame {
     private static final long serialVersionUID = 1L;
     private JTree tree;
     private Stack<TreeNode> stack;
     private String name;
+    private static int windowDimension;
 
     public TreeGUI(String name) {
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        TreeGUI.windowDimension = (int) Math.min(screenSize.width * 0.75, screenSize.height * 0.75);
         this.name = name;
         this.stack = new Stack<TreeNode>();
         DefaultMutableTreeNode root = new DefaultMutableTreeNode(name);
         this.tree = new JTree(root);
-        // DefaultTreeCellRenderer renderer = (DefaultTreeCellRenderer)
-        // tree.getCellRenderer();
-        // int dim = 20;
-        // ImageIcon closedIcon = new ImageIcon("./images/plus.png");
-        // closedIcon = new ImageIcon(closedIcon.getImage().getScaledInstance(dim, dim,
-        // java.awt.Image.SCALE_SMOOTH));
-        // ImageIcon openIcon = new ImageIcon("./images/minus.png");
-        // openIcon = new ImageIcon(openIcon.getImage().getScaledInstance(dim, dim,
-        // java.awt.Image.SCALE_SMOOTH));
-        // ImageIcon leafIcon = new ImageIcon("./images/leaf.png");
-        // leafIcon = new ImageIcon(leafIcon.getImage().getScaledInstance(dim, dim,
-        // java.awt.Image.SCALE_SMOOTH));
-        // renderer.setClosedIcon(closedIcon);
-        // renderer.setOpenIcon(openIcon);
-        // renderer.setLeafIcon(leafIcon);
+        DefaultTreeCellRenderer renderer = (DefaultTreeCellRenderer) tree.getCellRenderer();
+        int dim = 18;
+        ImageIcon closedIcon = new ImageIcon("./images/class.png");
+        closedIcon = new ImageIcon(closedIcon.getImage().getScaledInstance(dim, dim, java.awt.Image.SCALE_SMOOTH));
+        ImageIcon openIcon = new ImageIcon("./images/class.png");
+        openIcon = new ImageIcon(openIcon.getImage().getScaledInstance(dim, dim, java.awt.Image.SCALE_SMOOTH));
+        ImageIcon leafIcon = new ImageIcon("./images/class.png");
+        leafIcon = new ImageIcon(leafIcon.getImage().getScaledInstance(dim, dim, java.awt.Image.SCALE_SMOOTH));
+        renderer.setClosedIcon(closedIcon);
+        renderer.setOpenIcon(openIcon);
+        renderer.setLeafIcon(leafIcon);
         stack.add(new TreeNode(null, null, root));
+        this.tree.setToggleClickCount(0);
         JScrollPane scrollTree = new JScrollPane(this.tree);
         scrollTree.setViewportView(this.tree);
         scrollTree.setVisible(true);
@@ -47,14 +56,22 @@ public class TreeGUI extends JFrame {
                     return;
                 }
                 StackEvent nodeInfo = (StackEvent) node.getUserObject();
+                if (nodeInfo == null) {
+                    return;
+                }
                 JFrame popup = new JFrame();
-                String[] header = { "Method Name", "Return Value" };
-                String[][] values = { header, { nodeInfo.getEventMethod(), nodeInfo.getReturnValue() } };
+                String[] header = { "Method Name", "Return Value", "Execution Time" };
+                String[][] values = { header,
+                        { nodeInfo.getEventMethod(), nodeInfo.getReturnValue(), nodeInfo.getTime() + "ms" } };
                 JTable table = new JTable(values, header);
+                popup.getRootPane().registerKeyboardAction(event -> {
+                    popup.dispose();
+                }, KeyStroke.getKeyStroke(27, 0), JComponent.WHEN_IN_FOCUSED_WINDOW);
                 popup.add(table);
                 popup.setTitle(nodeInfo.getEventMethod());
                 popup.pack();
-                popup.setSize(500, popup.getHeight());
+                popup.setSize(TreeGUI.windowDimension, popup.getHeight());
+                popup.setLocationRelativeTo(null);
                 popup.setVisible(true);
                 System.out.println("Method: " + nodeInfo.getEventMethod() + ", Returned: " + nodeInfo.getReturnValue());
             }
@@ -63,6 +80,7 @@ public class TreeGUI extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Stack Trace: " + this.name);
         this.resizeToFit();
+        this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
 
@@ -87,11 +105,11 @@ public class TreeGUI extends JFrame {
     }
 
     private void resizeToFit() {
-        if (this.getWidth() < 500) {
-            this.setSize(500, this.getHeight());
+        if (this.getWidth() < TreeGUI.windowDimension) {
+            this.setSize(TreeGUI.windowDimension, this.getHeight());
         }
-        if (this.getHeight() < 500) {
-            this.setSize(this.getWidth(), 500);
+        if (this.getHeight() < TreeGUI.windowDimension) {
+            this.setSize(this.getWidth(), TreeGUI.windowDimension);
         }
     }
 
