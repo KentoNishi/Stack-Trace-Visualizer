51d016650e18bbd0ecc3e9c893102d2b2cf9c7d4
Kento Nishi <kento24gs@outlook.com>
2020-04-30 00:04:57 -0700

    Added method expansion

--- src/TreeGUI.java
+++ src/TreeGUI.java
@@ -2,7 +2,10 @@ import java.util.Stack;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTree;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.DefaultTreeModel;
@@ -18,22 +21,44 @@ public class TreeGUI extends JFrame {
         this.stack = new Stack<TreeNode>();
         DefaultMutableTreeNode root = new DefaultMutableTreeNode(name);
         this.tree = new JTree(root);
-        this.tree.setToggleClickCount(1);
-        DefaultTreeCellRenderer renderer = (DefaultTreeCellRenderer) tree.getCellRenderer();
-        int dim = 20;
-        ImageIcon closedIcon = new ImageIcon("./images/plus.png");
-        closedIcon = new ImageIcon(closedIcon.getImage().getScaledInstance(dim, dim, java.awt.Image.SCALE_SMOOTH));
-        ImageIcon openIcon = new ImageIcon("./images/minus.png");
-        openIcon = new ImageIcon(openIcon.getImage().getScaledInstance(dim, dim, java.awt.Image.SCALE_SMOOTH));
-        ImageIcon leafIcon = new ImageIcon("./images/leaf.png");
-        leafIcon = new ImageIcon(leafIcon.getImage().getScaledInstance(dim, dim, java.awt.Image.SCALE_SMOOTH));
-        renderer.setClosedIcon(closedIcon);
-        renderer.setOpenIcon(openIcon);
-        renderer.setLeafIcon(leafIcon);
+        // DefaultTreeCellRenderer renderer = (DefaultTreeCellRenderer)
+        // tree.getCellRenderer();
+        // int dim = 20;
+        // ImageIcon closedIcon = new ImageIcon("./images/plus.png");
+        // closedIcon = new ImageIcon(closedIcon.getImage().getScaledInstance(dim, dim,
+        // java.awt.Image.SCALE_SMOOTH));
+        // ImageIcon openIcon = new ImageIcon("./images/minus.png");
+        // openIcon = new ImageIcon(openIcon.getImage().getScaledInstance(dim, dim,
+        // java.awt.Image.SCALE_SMOOTH));
+        // ImageIcon leafIcon = new ImageIcon("./images/leaf.png");
+        // leafIcon = new ImageIcon(leafIcon.getImage().getScaledInstance(dim, dim,
+        // java.awt.Image.SCALE_SMOOTH));
+        // renderer.setClosedIcon(closedIcon);
+        // renderer.setOpenIcon(openIcon);
+        // renderer.setLeafIcon(leafIcon);
         stack.add(new TreeNode(null, null, root));
         JScrollPane scrollTree = new JScrollPane(this.tree);
         scrollTree.setViewportView(this.tree);
         scrollTree.setVisible(true);
+        this.tree.addTreeSelectionListener(new TreeSelectionListener() {
+            public void valueChanged(TreeSelectionEvent e) {
+                DefaultMutableTreeNode node = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
+                if (node == null) {
+                    return;
+                }
+                StackEvent nodeInfo = (StackEvent) node.getUserObject();
+                JFrame popup = new JFrame();
+                String[] header = { "Method Name", "Return Value" };
+                String[][] values = { header, { nodeInfo.getEventMethod(), nodeInfo.getReturnValue() } };
+                JTable table = new JTable(values, header);
+                popup.add(table);
+                popup.setTitle(nodeInfo.getEventMethod());
+                popup.pack();
+                popup.setSize(500, popup.getHeight());
+                popup.setVisible(true);
+                System.out.println("Method: " + nodeInfo.getEventMethod() + ", Returned: " + nodeInfo.getReturnValue());
+            }
+        });
         this.add(scrollTree);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Stack Trace: " + this.name);
@@ -46,7 +71,7 @@ public class TreeGUI extends JFrame {
     }
 
     private DefaultMutableTreeNode newNode(StackEvent event) {
-        DefaultMutableTreeNode node = new DefaultMutableTreeNode(event.getEventMethod());
+        DefaultMutableTreeNode node = new DefaultMutableTreeNode(event);
         return node;
     }
 
